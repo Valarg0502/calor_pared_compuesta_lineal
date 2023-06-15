7ad39c916d964a060e0439dfcc7cca723f44ef15
Valarg0502 <valentina.arguellez@gmail.com>
2023-06-15 00:09:17 -0500

    Ajustando setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
+import pathlib
 from setuptools import find_packages, setup
 
 readme = open("./README.md", "r")
-
-
+HERE = pathlib.Path(__file__).parent
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8')
 setup(
     name='calor_pared_compuesta_lineal',
-    packages=['calor_pared_compuesta_lineal'],  # this must be the same as the name above
+    # packages=['calor_pared_compuesta_lineal'],  # this must be the same as the name above
     version='0.0.1',
     description='Esta es la descripcion de mi paquete',
-    long_description=readme.read(),
+    long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     author='Valentina Argüellez Angulo',
     author_email='valentina.arguellez@gmail.com',
@@ -19,7 +20,8 @@ setup(
     classifiers=[ ],
     install_requires=[
         'numpy',
-        'matplotlib'
+        'matplotlib',
+        'calor_pared_compuesta_lineal'
     ],
     license='MIT',
     packages=find_packages(),
